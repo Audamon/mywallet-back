c204b6046d601e0dec2d25d4fa138f6ff1a7ea03
Audamon <joaotamagno@gmail.com>
2021-10-25 17:00:39 -0300

    test

--- validate.js
+++ validate.js
@@ -2,8 +2,8 @@ import joi from 'joi';
 
 const val =  joi.object({
     username: joi.string(), 
-    email: joi.string(), 
-    password:joi.string().min(8).pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/))
+    email: joi.string().pattern(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)), 
+    password: joi.string().min(8).pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/))
 }).length(3);
 
 export {val}
