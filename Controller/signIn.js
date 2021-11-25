e487e1aee809e57a6eeb2ef5373621d421920f3d
Audamon <joaotamagno@gmail.com>
2021-11-25 18:50:19 -0300

    SignIn test was implemented

--- Controller/signIn.js
+++ Controller/signIn.js
@@ -1,10 +1,14 @@
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import connection from '../database.js';
+import { signInSchema } from '../Schemas/schemas.js';
 
 async function signIn(req, res) {
   const { email, password } = req.body;
-
+  const validate = signInSchema.validate(req.body);
+  if (validate.error) {
+    return res.sendStatus(400);
+  }
   try {
     const user = await connection.query(
       `
@@ -12,6 +16,9 @@ async function signIn(req, res) {
           `,
       [email]
     );
+    if(user.rowCount === 0){
+      return res.sendStatus(401 );
+    }
     const hash = bcrypt.compareSync(password, user.rows[0].password);
     if (!hash) {
       return res.sendStatus(401);
